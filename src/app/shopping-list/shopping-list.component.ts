5524a283365f8a05102d7a5b5b5d2ecf30937ed6
firzen92 <vinay.potaraju@gmail.com>
2021-06-21 15:43:26 +0530

    removed comments
    rc

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -13,7 +13,6 @@ import { Store } from '@ngrx/store';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Observable <{ingredients: Ingredient[]}>;
-  private subscription: Subscription;
 
   constructor(
     private slService: ShoppingListService,
@@ -23,21 +22,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.store.select('shoppingList');
-    // this.ingredients = this.slService.getIngredients();
-    // this.subscription = this.slService.ingredientsChanged.subscribe(
-    //   (ingredients: Ingredient[]) => {
-    //     this.ingredients = ingredients;
-    //   }
-    // );
-
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
   }
 
   onEditItem(index: number) {
     this.slService.startedEditing.next(index);
   }
-
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe();
-  // }
 }
